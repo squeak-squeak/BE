90861ee4799e0600c78b1dd8754d821444708f57
MinjuKwak01 <mkwak1125@gmail.com>
2025-01-19 01:55:53 +0900

    fix : 환경변수 명칭 수정

--- squeak-squeak/src/main/java/com/be/squeak_squeak/member/oauth/naver/NaverApiClient.java
+++ squeak-squeak/src/main/java/com/be/squeak_squeak/member/oauth/naver/NaverApiClient.java
@@ -9,14 +9,14 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 @Component
 public class NaverApiClient {
-    @Value("${naver.access-token.url}")
+    @Value("${naver.access-token-url}")
     private String NAVER_ACCESS_TOKEN_URL;
-    @Value("${naver.member-info.url}")
+    @Value("${naver.member-info-url}")
     private String NAVER_MEMBER_INFO_URL;
     @Value("${naver.client-id}")
     private String NAVER_CLIENT_ID;
-    @Value("${naver.client-secret}")
-    private String NAVER_CLIENT_SECRET;
+    @Value("${naver.secret-id}")
+    private String NAVER_SECRET_ID;
 
     public NaverMemberInfoRes getUserInfo(String accessToken) {
         return WebClient.create(NAVER_MEMBER_INFO_URL).post()
@@ -39,8 +39,8 @@ public class NaverApiClient {
                         .path("/oauth2.0/token")
                         .queryParam("grant_type", "authorization_code")
                         .queryParam("client_id", NAVER_CLIENT_ID)
-                        .queryParam("client_secret", NAVER_CLIENT_SECRET)
                         .queryParam("code", code)
+                        .queryParam("client_secret", NAVER_SECRET_ID)
                         .queryParam("state", state)
                         .build(true))
                 .retrieve()

1cd28fdb54450b84b4ba6f032554fae5e9ccea5d
MinjuKwak01 <mkwak1125@gmail.com>
2025-01-19 01:32:02 +0900

    fix : 모든 변수에 @JsonProperty 붙여서 통일

--- squeak-squeak/src/main/java/com/be/squeak_squeak/member/oauth/kakao/dto/KakaoMemberInfoRes.java
+++ squeak-squeak/src/main/java/com/be/squeak_squeak/member/oauth/kakao/dto/KakaoMemberInfoRes.java
@@ -4,23 +4,29 @@ package com.be.squeak_squeak.member.oauth.kakao.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record KakaoMemberInfoRes(
+        @JsonProperty("id")
         String id,
         @JsonProperty("connected_at")
         String connectedAt,
+        @JsonProperty("properties")
         Properties properties,
         @JsonProperty("kakao_account")
         KakaoAccount kakaoAccount
 ) {
     public record Properties(
+            @JsonProperty("nickname")
             String nickname
     ) {
     }
 
     public record KakaoAccount(
+            @JsonProperty("profile")
             Profile profile,
+            @JsonProperty("email")
             String email
     ) {
         public record Profile(
+                @JsonProperty("nickname")
                 String nickname
         ) {
         }

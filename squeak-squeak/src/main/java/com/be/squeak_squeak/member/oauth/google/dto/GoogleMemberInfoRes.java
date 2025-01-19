9da581c6db0d0aaa3ba3fd558f1d2d6e41c2f044
hanwldn76 <hanwldn76@gmail.com>
2025-01-19 18:38:13 +0900

    fix : dto 매핑 문제 해결

--- squeak-squeak/src/main/java/com/be/squeak_squeak/member/oauth/google/dto/GoogleMemberInfoRes.java
+++ squeak-squeak/src/main/java/com/be/squeak_squeak/member/oauth/google/dto/GoogleMemberInfoRes.java
@@ -3,8 +3,8 @@ package com.be.squeak_squeak.member.oauth.google.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record GoogleMemberInfoRes (
-        @JsonProperty("id") String id,
-        @JsonProperty("email") String email,
-        @JsonProperty("name") String name
+        @JsonProperty("sub") String id,
+        @JsonProperty("name") String name,
+        @JsonProperty("email") String email
 ){
 }

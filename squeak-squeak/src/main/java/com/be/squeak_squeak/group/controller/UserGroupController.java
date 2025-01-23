50025049d89b583581249d486f5e4cecf5134bd7
hanwldn76 <hanwldn76@gmail.com>
2025-01-23 21:38:47 +0900

    feat : controller, service, repository 틀만 생성

---
+++ squeak-squeak/src/main/java/com/be/squeak_squeak/group/controller/UserGroupController.java
@@ -0,0 +1,9 @@
+package com.be.squeak_squeak.group.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class UserGroupController {
+}
---
+++ squeak-squeak/src/main/java/com/be/squeak_squeak/group/service/UserGroupService.java
@@ -0,0 +1,9 @@
+package com.be.squeak_squeak.group.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserGroupService {
+}

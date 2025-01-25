369cba0bde5a12ea6873b5b56713bddcc8149d52
MinjuKwak01 <mkwak1125@gmail.com>
2025-01-25 15:25:50 +0900

    feat : socialuuid를 담을 MemberInfo 클래스

---
+++ squeak-squeak/src/main/java/com/be/squeak_squeak/common/auth/MemberInfo.java
@@ -0,0 +1,13 @@
+package com.be.squeak_squeak.common.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MemberInfo {
+    private String socialUuid;
+}

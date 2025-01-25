06cd43cbc197d645148c1faa4327f2a449483282
MinjuKwak01 <mkwak1125@gmail.com>
2025-01-25 22:38:58 +0900

    feat : 검색 및 필터링 기능 구현

--- squeak-squeak/src/main/java/com/be/squeak_squeak/group/service/UserGroupService.java
+++ squeak-squeak/src/main/java/com/be/squeak_squeak/group/service/UserGroupService.java
@@ -1,7 +1,9 @@
 package com.be.squeak_squeak.group.service;
 
+import com.be.squeak_squeak.common.auth.MemberInfo;
 import com.be.squeak_squeak.group.dto.CreateGroupReq;
 import com.be.squeak_squeak.group.dto.CreateGroupRes;
+import com.be.squeak_squeak.group.dto.SearchGroupRes;
 import com.be.squeak_squeak.group.dto.UpdateGroupReq;
 import com.be.squeak_squeak.group.dto.UpdateGroupRes;
 import com.be.squeak_squeak.group.entity.UserGroup;
@@ -11,11 +13,12 @@ import com.be.squeak_squeak.groupMember.entity.MemberStatus;
 import com.be.squeak_squeak.groupMember.repository.GroupMemberRepository;
 import com.be.squeak_squeak.member.entity.Member;
 import com.be.squeak_squeak.member.repository.MemberRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.security.SecureRandom;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -23,14 +26,14 @@ public class UserGroupService {
     private final UserGroupRepository userGroupRepository;
     private final GroupMemberRepository groupMemberRepository;
     private final MemberRepository memberRepository;
+    private final UserGroupCustomRepository userGroupCustomRepository;
 
     public CreateGroupRes createGroup(CreateGroupReq request, Long memberId) {
         // 초대 코드 생성
-         String inviteCode = generateInviteCode();
-
+        String inviteCode = generateInviteCode();
 
         Member creator = memberRepository.findById(memberId)
-                            .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 회원입니다."));
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 회원입니다."));
 
         UserGroup group = UserGroup.builder()
                 .name(request.name())
@@ -112,10 +115,29 @@ public class UserGroupService {
                 .build();
     }
 
+    public List<SearchGroupRes> searchGroup(MemberInfo memberInfo, String type, String keyword) {
+        Member member = memberRepository.findById(memberInfo.getId())
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 회원입니다."));
+        // 그룹장인 그룹만 검색
+        List<UserGroup> userGroupList = userGroupCustomRepository.searchGroup(type, keyword, member.getId());
+
+        return userGroupList.stream()
+                .map(group -> SearchGroupRes.builder()
+                        .id(group.getId())
+                        .image(group.getImage())
+                        .name(group.getName())
+                        .build())
+                .toList();
+    }
+
     public void checkOwnerPermission(UserGroup group, Member member) {
-        boolean isOwner = groupMemberRepository.findByUserGroupAndMemberAndStatus(group, member, MemberStatus.OWNER).isPresent();
+        boolean isOwner = groupMemberRepository.findByUserGroupAndMemberAndStatus(group, member, MemberStatus.OWNER)
+                .isPresent();
         if (!isOwner) {
             throw new IllegalStateException("이 작업은 OWNER만 수행할 수 있습니다.");
         }
     }
+
+
 }
+

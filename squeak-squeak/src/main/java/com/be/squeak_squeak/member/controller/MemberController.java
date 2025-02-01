233bd10f3aefde283252cb2b08b7f3046deedb4f
MinjuKwak01 <mkwak1125@gmail.com>
2025-02-02 01:04:21 +0900

    fix : 요청 url /api로 통일

--- squeak-squeak/src/main/java/com/be/squeak_squeak/member/controller/MemberController.java
+++ squeak-squeak/src/main/java/com/be/squeak_squeak/member/controller/MemberController.java
@@ -15,10 +15,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/member")
 public class MemberController {
 
     private final OAuthService oauthService;
